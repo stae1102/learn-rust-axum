df1068b4ea4bdb317f6d59830c1a2c20957d488f
Seongtae <stae1102@gmail.com>
2023-06-05 14:36:47 +0900

    feat: Route plain_text and json contents

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,28 @@
 use axum::{
-    Router,
+    body::Body,
     routing::get,
-    extract::{Path, Query, Json},
+    response::Json,
+    Router,
 };
+use serde_json::{Value, json};
+// use serde_json::{Value, json};
 
-use std::collections::HashMap;
+// &'static str becomes a 200 OK with content-type: text/plain; charset=utf-8.
+async fn plain_text() -> &'static str {
+    "foo"
+}
 
-// 'Path' gives you the path parameters and deserializes them.
-async fn path(Path(user_id): Path<u32>) {}
-
-// 'Query' gives you the query parameters and deserializes them.
-async fn query(Query(params): Query<HashMap<String, String>>) {}
-
-// Buffer the request body and deserialize it as JSON into a
-// 'serde_json::Value'. 'Json' supports any type that implements
-// 'serde::Deserilaize'.
-async fn json(Json(payload): Json<serde_json::Value>) {}
+// Json gives a content-type of 'application/json and works with any type
+// that implements serde::Serialize
+async fn json() -> Json<Value> {
+    Json(json!({ "data": 42 }))
+}
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
-        .route("/", get(root))
-        .route("/foo", get(get_foo).post(post_foo))
-        .route("/foo/bar", get(foo_bar));
+        .route("/plain_text", get(plain_text))
+        .route("/json", get(json));
 
     axum::Server::bind(&"127.0.0.1:8000".parse().unwrap())
         .serve(app.into_make_service())
@@ -34,6 +34,3 @@ async fn main() {
 async fn root() -> String {
     "Hello World!".to_string()
 }
-async fn get_foo() {}
-async fn post_foo() {}
-async fn foo_bar() {}
