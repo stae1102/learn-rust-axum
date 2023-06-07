346edd6769dc627c25aa8b6dc618459ac0ab0d1d
Seongtae <stae1102@gmail.com>
2023-06-07 22:32:04 +0900

    feat: Set html page

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,18 @@
 use axum::{
-    body::Body,
     routing::get,
-    response::Json,
+    response::{Json, Html},
     Router,
 };
-use serde_json::{Value, json};
-// use serde_json::{Value, json};
-
-// &'static str becomes a 200 OK with content-type: text/plain; charset=utf-8.
-async fn plain_text() -> &'static str {
-    "foo"
-}
-
-// Json gives a content-type of 'application/json and works with any type
-// that implements serde::Serialize
-async fn json() -> Json<Value> {
-    Json(json!({ "data": 42 }))
-}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/plain_text", get(plain_text))
-        .route("/json", get(json));
+    let routes_hello: Router = Router::new()
+        .route("/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
 
     axum::Server::bind(&"127.0.0.1:8000".parse().unwrap())
-        .serve(app.into_make_service())
+        .serve(routes_hello.into_make_service())
         .await
         .unwrap();
-}
-
-// calls one of these handlers
-async fn root() -> String {
-    "Hello World!".to_string()
 }
