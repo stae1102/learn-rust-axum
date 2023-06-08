0217f48c19a447ff671df4e48ef5feff5c40b437
Seongtae <stae1102@gmail.com>
2023-06-09 01:39:01 +0900

    feat: Add response mapper

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ pub use self::error::{Error, Result};
 
 use axum::{
     routing::{get, get_service},
-    response::{Html, IntoResponse},
-    Router, extract::{Query, Path},
+    response::{Html, IntoResponse, Response},
+    Router, extract::{Query, Path}, middleware,
 };
 use serde::Deserialize;
 use tower_http::services::ServeDir;
@@ -15,7 +15,11 @@ mod web;
 
 #[tokio::main]
 async fn main() {
-    let routes_all: Router = Router::new().merge(routes_hello()).merge(web::routes_login::routes()).fallback_service(routes_static());
+    let routes_all: Router = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .fallback_service(routes_static());
 
     // region:      --- Start Server
     let addr = SocketAddr::from(([127, 0, 0, 1], 8001));
@@ -24,6 +28,13 @@ async fn main() {
         .serve(routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+
+    println!();
+    res
 }
 
 fn routes_static() -> Router {
